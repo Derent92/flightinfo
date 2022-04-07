a57092eb1935d102388b39db6652e52360ee4b7e
Derent92 <simonp_92@gmx.de>
2022-04-07 13:58:29 +0000

    lastest version 07.04.2022

--- webapp/controller/FlightList.controller.js
+++ webapp/controller/FlightList.controller.js
@@ -1,16 +1,29 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, History) {
         "use strict";
 
         return Controller.extend("flightinfo.controller.FlightList", {
             onInit: function () {
             
             },
+
+            onNavBack: function( ){
+                var oHistory = History.getInstance();
+                var sPrevHash = oHistory.getPreviousHash();
+
+                if ( sPrevHash !== undefined ) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("CarrierList", true);
+                }
+            }
 
 
 
